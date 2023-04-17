fbd27907c8263bb52bd7e9d640ee409a3bb8cd2b
Ranjani Venkatesh <ranjani@email.unc.edu>
2023-04-17 13:53:26 -0400

    send 200 ok

--- server.js
+++ server.js
@@ -5,4 +5,13 @@ import express from 'express';
 import minimist from 'minimist';
 
 const argv = minimist(process.argv.slice(2));
+const port = argv.port || 5000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//check endpoint at /app/ that returns 200 ok
+app.get('/app', (req, res)) => {
+    res.status(200).send("200 OK");
+}
 
