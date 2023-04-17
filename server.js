be2b6ee88f382e58a6c63fe287cd855fbf7f00e1
Ranjani Venkatesh <ranjani@email.unc.edu>
2023-04-17 14:01:29 -0400

    check

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node 
 
-import { rps, rpsls } from "../lib/rpsls.js";
+import { rps, rpsls } from "./lib/rpsls.js";
 import express from 'express';
 import minimist from 'minimist';
 
